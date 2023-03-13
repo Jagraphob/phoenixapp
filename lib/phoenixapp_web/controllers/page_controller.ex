e66255428e9e66592981cbb11e0298812621f4a0
TohJantakananuruk <toh@hatchinvest.nz>
2023-03-13 15:51:21 +1300

    generated phoenix app

---
+++ lib/phoenixapp_web/controllers/page_controller.ex
@@ -0,0 +1,10 @@
+defmodule PhoenixappWeb.PageController do
+  use PhoenixappWeb, :controller
+
+  @spec home(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
